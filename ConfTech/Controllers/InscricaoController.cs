faf8c35797f75faecaeb84d8b2c6f6c7795cf9de
ingridigavem <58342622+ingridigavem@users.noreply.github.com>
2021-07-22 11:15:16 -0300

    Adicionar arquivos de projeto.

---
+++ ConfTech/Controllers/InscricaoController.cs
@@ -0,0 +1,46 @@
+﻿using ConfTech.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConfTech.Controllers {
+    public class InscricaoController : Controller {
+
+        private IMongoCollection<FormularioModel> _inscricao;
+
+        public InscricaoController(IMongoClient client) {
+
+            var database = client.GetDatabase("confTech");
+            _inscricao = database.GetCollection<FormularioModel>("inscricao");
+        }
+
+        // GET: InscricaoController
+        public ActionResult Index() {
+            return View();
+        }
+
+        public ActionResult InscricaoRealizada() {   
+            return View();
+        }
+
+        [HttpGet]
+        public IEnumerable<FormularioModel> GetInscricoes() {
+            return _inscricao.Find(c => true).ToList();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(FormularioModel modelForm) {
+            if (modelForm.Checkbox == null) modelForm.Checkbox = "off";
+            if (!modelForm.InscricaoValida(modelForm.Nome, modelForm.Email)) return RedirectToAction(nameof(Index));               
+            
+            _inscricao.InsertOne(modelForm);
+            return RedirectToAction(nameof(InscricaoRealizada));                         
+        }
+
+    }
+}
---
+++ ConfTech/Models/FormularioModel.cs
@@ -0,0 +1,43 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConfTech.Models {
+    public class FormularioModel {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [Required(ErrorMessage = "Preenchimento Obrigatório")]
+        [BsonRequired]
+        [BsonElement("nome")]
+        public string Nome { get; set; }
+
+        [BsonRequired]
+        [BsonElement("email")]
+        public string Email { get; set; }
+
+        [BsonElement("profissao")]
+        public string Profissao { get; set; }
+
+        [BsonElement("experiencia")]
+        public string Experiencia { get; set; }
+
+        [BsonElement("checkbox")]
+        public string Checkbox { get; set; }
+
+        public bool InscricaoValida(string nome, string email) {
+            Nome = nome;
+            Email = email;
+            if (Nome == null || Email == null) {
+                return false;
+            }
+            return true;
+        }
+
+    }
+}
