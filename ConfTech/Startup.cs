1e4e20da69a4ed20140c3c21f6a05351e5be2d99
ingridigavem <58342622+ingridigavem@users.noreply.github.com>
2021-07-22 12:05:35 -0300

    Update Startup.cs

--- ConfTech/Startup.cs
+++ ConfTech/Startup.cs
@@ -48,7 +48,7 @@ namespace ConfTech {
             app.UseEndpoints(endpoints => {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=ConfTech}/{action=Index}/{id?}");
             });
         }
     }
